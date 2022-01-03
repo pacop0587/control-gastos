a74daa5b62b76b84220b0fbaef5de61dca9b62de
Paco <pacop0587@gmail.com>
2022-01-03 14:37:47 -0600

    Se agrego la funcion para validar formulario y se creo componente Mensaje, que muestra el mensaje de error en caso de no llenar correctamente el formulario

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development';
+import Mensaje from './Mensaje';
+
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+    //Hooks useState
+    const [mensaje, setMensaje] = useState('');
+
+    const validarFormulario = (e) =>{
+        e.preventDefault();
+
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje('No es un presupuesto valido')
+        }else{
+            console.log('Si es un presupuesto valido')
+        }
+    }
+
+
     return (
         <div className='contenedor-presupuesto contenedor sombra'>
-            <form action="" className='formulario'>
+            <form onSubmit={validarFormulario} className='formulario'>
                 <div className="campo">
                     <label htmlFor="">Definir Presupuesto</label>
                     <input 
@@ -16,6 +33,7 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
                 </div>
                 <input 
                 type="submit" value="Añadir" />
+                {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje> }
             </form>
         </div>
     )
